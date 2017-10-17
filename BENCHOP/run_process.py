574242d612a3fcbfa881ad7ee3f3a0d64e71e721
Filip <filip.c.e.johansson@gmail.com>
2017-10-18 00:27:40 +0200

    update

--- BENCHOP/run_process.py
+++ BENCHOP/run_process.py
@@ -1,21 +1,10 @@
-#from oct2py import octave
 from oct2py import Oct2Py, get_log
-#import numpy as np
 import json, requests, time, sys
 
-#octave.addpath('/home/ubuntu/acc-proj/BENCHOP/')
-#octave.eval('quicktable()')
-#octave.quicktable()
-#octave.feval('/home/ubuntu/acc-proj/BENCHOP/quicktable.m')
-#oc = Oct2Py(logger=get_log())
-#oc.logger = (get_log('new_log')
-#oc.logger.setLevel(logging.INFO)
-#octave.feval('/home/ubuntu/acc-proj/BENCHOP/add.m', 1, 2)
-#Oct2Py.feval('/home/ubuntu/acc-proj/BENCHOP/add.m', 1, 2)
 def run(problem, method):
 	oc = Oct2Py()
-	#x = oc.add(2, 2)
 	x = oc.qtable2(problem, method)
+	# qtable2(1,'COS')
 	return x
 
 
@@ -24,17 +13,16 @@ if __name__ =="__main__":
 	print(sys.argv)
 	print(len(sys.argv))
 	if(len(sys.argv) == 4):
-		problem = sys.argv[1] if sys.argv[1] else 'none'
-		method = sys.argv[2] if sys.argv[2] else 'none'
-		api = sys.argv[3] if sys.argv[3] else '0.0.0.0'
+		problem = sys.argv[1]
+		method = sys.argv[2] if sys.argv[2]
+		api = sys.argv[3]
 		api = 'http://' + api + ':5000/api/v1/result'
-		result = run(problem, method)
 		result = run(problem, method)
 		finished = time.time()
 		resultTime = finished - start
 		print('running problem: ' + problem + ' method: ' + method + ' result: ' + str(result) + ' time: ' + str(resultTime) + ' target api: ' + api)
 		try:
-			r = requests.post(api, data={'problem': problem, 'method': method, 'time': resultTime, 'result': result})
+			r = requests.post(api, data={'problem': problem, 'method': method, 'time': resultTime, 'result': result, 'id': '7b1d5f1d-d293-46c3-8883-f809344663ad'})
 		except requests.exceptions.RequestException as e:
 			print (e)
 			sys.exit(1)
