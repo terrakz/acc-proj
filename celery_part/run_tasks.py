523fe03c68371f450e6f898af794a0e42cb69036
Daniel <ismat.halabi@telia.com>
2017-10-18 01:31:23 +0000

    added group in run_tasks.py

--- celery_part/run_tasks.py
+++ celery_part/run_tasks.py
@@ -1,12 +1,15 @@
 import time
 from .celery_app import * #run_table 
+from celery import group
 
 if __name__ == '__main__':
  #for _ in range(1):
-      result = run_table.apply_async()
-      while not result.ready():
-        print('sleeping 1 sec')
-        time.sleep(1)
+      #result = run_table.apply_async()
+      #while not result.ready():
+      #  print('sleeping 1 sec')
+      #  time.sleep(1)
  
+     # print (result.result)
 
-      print (result.result)
+     result = group(run_table())
+     result.get(36000)
